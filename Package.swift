b59448bdc19c87524b9d5f08971a0b0b9894f757
koen hendrikx <koenh1@gmail.com>
2023-06-15 20:22:13 +0200

    set versions

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ let package = Package(
     name: "TensorPackage",
     platforms: [
         .macOS(.v13),
+        .macCatalyst(.v16),
+        .iOS(.v16)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
